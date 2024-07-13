5ecadc050501e71a4cdea620486ef1745725f12d
lukachi <lukachisama@gmail.com>
2024-07-13 23:27:27 +0300

    setup eslint & prettier wip

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Options} */
+const config = {
+    tabWidth: 2,
+    printWidth: 100,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: "avoid",
+    trailingComma: "all"
+};
+
+export default config;
