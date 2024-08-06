175a0074f428bfe598132156a9c77da6950db5db
lukachi <lukachisama@gmail.com>
2024-08-06 22:48:03 +0300

    update deps, rebuild app

--- src/utils/zkp/enums.ts
+++
@@ -1,7 +0,0 @@
-export enum WitnessRequestTypes {
-  WitnessCalculatorRequest = 'witness_calculator_request',
-}
-
-export enum WitnessResponseTypes {
-  WitnessCalculatorResponse = 'witness_calculator_response',
-}
