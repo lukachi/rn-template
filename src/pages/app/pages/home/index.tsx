50dc744f454b5a72b321c50d5ac9e59a14dc9a92
lukachi <lukachisama@gmail.com>
2025-02-14 00:38:33 +0200

    add pagination with custom animation

--- src/pages/app/pages/home/index.tsx
+++ src/pages/app/pages/home/index.tsx
@@ -1,14 +1,14 @@
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { useState } from 'react'
 import { Dimensions, Pressable, Text, View } from 'react-native'
-import Animated, { useSharedValue } from 'react-native-reanimated'
-import Carousel from 'react-native-reanimated-carousel'
+import Animated, { Extrapolation, interpolate, useSharedValue } from 'react-native-reanimated'
+import Carousel, { Pagination } from 'react-native-reanimated-carousel'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import AppContainer from '@/pages/app/components/AppContainer'
 import type { AppTabScreenProps } from '@/route-types'
-import { useAppPaddings } from '@/theme'
-import { UiScreenScrollable } from '@/ui'
+import { useAppPaddings, useAppTheme } from '@/theme'
+import { UiIcon, UiScreenScrollable } from '@/ui'
 
 import { parallaxLayout } from './helpers/parallax'
 
@@ -18,6 +18,8 @@ const defaultDataWith6Colors = ['#B0604D', '#899F9C', '#B3C680', '#5C6265', '#F5
 
 // eslint-disable-next-line no-empty-pattern
 export default function HomeScreen({}: AppTabScreenProps<'Home'>) {
+  const { palette } = useAppTheme()
+
   const insets = useSafeAreaInsets()
   const appPaddings = useAppPaddings()
   const bottomBarHeight = useBottomTabBarHeight()
@@ -26,20 +28,15 @@ export default function HomeScreen({}: AppTabScreenProps<'Home'>) {
   const [containerHeight, setContainerHeight] = useState(0)
 
   return (
-    <AppContainer
-      style={{
-        backgroundColor: 'red',
-      }}
-    >
+    <AppContainer>
       <UiScreenScrollable
         style={{
-          backgroundColor: 'green',
           paddingBottom: bottomBarHeight,
         }}
         className='gap-3'
       >
         <View
-          className='flex flex-1 bg-blue-700'
+          className='flex flex-1'
           style={{
             paddingTop: insets.top,
           }}
@@ -51,10 +48,17 @@ export default function HomeScreen({}: AppTabScreenProps<'Home'>) {
               paddingRight: appPaddings.right,
             }}
           >
-            <Text className={'text-textPrimary'}>Hi Stranger</Text>
+            <Text className={'!font-normal text-textPrimary typography-h5'}>Hi Stranger</Text>
 
-            <Pressable className={'size-10 rounded-full bg-backgroundContainer'}>
-              <View></View>
+            <Pressable className={'relative size-10 rounded-full bg-backgroundContainer'}>
+              <UiIcon
+                libIcon={'FontAwesome'}
+                name={'user-o'}
+                size={16}
+                className={
+                  'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-textPrimary'
+                }
+              />
             </Pressable>
           </View>
           <View
@@ -105,6 +109,44 @@ export default function HomeScreen({}: AppTabScreenProps<'Home'>) {
                 }}
               />
             )}
+
+            <Pagination.Custom<{ color: string }>
+              progress={progress}
+              data={defaultDataWith6Colors.map(color => ({ color }))}
+              dotStyle={{
+                width: 6,
+                height: 6,
+                borderRadius: 999,
+                backgroundColor: palette.backgroundContainer,
+              }}
+              activeDotStyle={{
+                overflow: 'hidden',
+                height: 16,
+                backgroundColor: palette.textPrimary,
+              }}
+              containerStyle={{
+                position: 'absolute',
+                right: 10,
+                top: '50%',
+                transform: 'translateY(-50%)',
+                gap: 8,
+              }}
+              horizontal={false}
+              customReanimatedStyle={(progress, index, length) => {
+                let val = Math.abs(progress - index)
+                if (index === 0 && progress > length - 1) {
+                  val = Math.abs(progress - length)
+                }
+
+                return {
+                  transform: [
+                    {
+                      translateY: interpolate(val, [0, 1], [0, 0], Extrapolation.CLAMP),
+                    },
+                  ],
+                }
+              }}
+            />
           </View>
         </View>
       </UiScreenScrollable>
