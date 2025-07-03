98e586497cf98b7857b693d20f846784a227ffad
lukachi <lukachisama@gmail.com>
2025-07-03 18:30:55 +0300

    Refactor Noir module and integrate SwoirenbergLib framework
    - Removed Swoir and SwoirCore podspecs as they are no longer needed.
    - Updated Noir.podspec to include vendored_frameworks for Swoirenberg.xcframework.
    - Added new Swift files for Circuit, Swoir, and SwoirCore implementations.
    - Introduced error handling and manifest parsing in Circuit class.
    - Implemented ABI and ABI_Parameter structures for circuit definitions.
    - Created SwoirBackendProtocol for backend operations related to proving and verifying circuits.
    - Added Info.plist and module.modulemap files for SwoirenbergLib framework.
    - Updated app.config.ts to comment out unused dependencies related to Swoir and SwoirCore.

---
+++ modules/noir/ios/Frameworks/SwoirenbergLib.xcframework/ios-arm64-simulator/SwoirenbergLib.framework/Headers/SwoirenbergLib.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe2beb17115345f8e40da8750d415298a83a007e2aecdb1c905d1b3312f874b2
+size 44
---
+++ modules/noir/ios/Frameworks/SwoirenbergLib.xcframework/ios-arm64/SwoirenbergLib.framework/Headers/SwoirenbergLib.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8429db56af247f9fce7967a1368a2f012216f5a91f6e2a8aa6a01d95a3f14e0
+size 84
